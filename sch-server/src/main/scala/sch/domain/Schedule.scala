b534d82b5aefc0e1617b5e7fa61a16a025f39327
AbdalimovArtur <a.abdalimov.97@gmail.com>
2018-01-28 04:36:23 +0600

    Update Schedule is now makes precalculation on room and start time

--- sch-server/src/main/scala/sch/domain/Schedule.scala
+++ sch-server/src/main/scala/sch/domain/Schedule.scala
@@ -7,16 +7,33 @@ import scala.util.Random
 
 class Schedule() {
 
-  var timeSlots: ListBuffer[ListBuffer[sch.domain.Class.Class]] = ListBuffer.fill(Constants.NUMBER_OF_ROOMS *
+  var timeSlots: ListBuffer[ListBuffer[sch.domain.Class.ClassResponse]] = ListBuffer.fill(Constants.NUMBER_OF_ROOMS *
                                                                                   Constants.WORK_DAYS *
                                                                                   Constants.AMOUNT_OF_HOURS)(ListBuffer())
-  var scheduleHash: Map[sch.domain.Class.Class, Int] = Map()
+  var scheduleHash: Map[sch.domain.Class.ClassResponse, Int] = Map()
 
   def addClass(newClass: sch.domain.Class.Class): Unit = {
 
     val timeAndPlace = Random.nextInt(timeSlots.size)
-    timeSlots(timeAndPlace) += newClass
-    scheduleHash += (newClass -> timeAndPlace)
+
+    val roomId = ((timeAndPlace % (Constants.AMOUNT_OF_HOURS * Constants.NUMBER_OF_ROOMS)) / 12).toString
+    val startTime = timeAndPlace % 11
+
+    val classResponse = sch.domain.Class.ClassResponse(
+                                            newClass.teacherId,
+                                            newClass.courseId,
+                                            newClass.hours,
+                                            newClass.groupId,
+                                            newClass.equiped,
+                                            roomId,
+                                            startTime.toString)
+
+    for (time <- 0 until classResponse.hours) {
+      if (startTime + 9 + time < 21)
+        timeSlots(timeAndPlace + time) += classResponse
+    }
+
+    scheduleHash += (classResponse -> timeAndPlace)
   }
 
   def mutate(anotherSchedule: Schedule): Schedule = {
@@ -41,7 +58,8 @@ class Schedule() {
     schedule
   }
 
-  /*def calculateFitnessRate(): Float = {
+  def calculateFitnessRate(): Float = {
+    var totalScore: Int = 0
 
-  }*/
+  }
 }
